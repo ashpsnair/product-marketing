31cd7fd48492eb33c7ddb86baf500c87e68f2d55
Aiswarya P.S <69599173+ashpsnair@users.noreply.github.com>
2021-06-17 12:58:47 +0530

    codebase update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Main from './Components/Main/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Navbar/>
+      </header>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
